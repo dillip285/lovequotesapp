cd8533fb456d3da380d96aa315581cfcb7248b9e
Dillip Das <dillip285@gmail.com>
2024-01-22 17:36:26 +0530

    Refined a little bit.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Love Quote Generator</h1>
         <QuoteGenerator />
       </header>
     </div>
--- src/components/QuoteGenerator/QuoteGenerator.js
+++ src/components/QuoteGenerator/QuoteGenerator.js
@@ -11,13 +11,13 @@ const QuoteGenerator = () => {
   };
 
   return (
-      <div className="QuoteGenerator">
+    <div className="QuoteGenerator">
       <h1>Love Quote Generator</h1>
       <p className="disclaimer">
-        Love your wife! but her love language is words of affirmation, and you're not sure where to start? Use this generator to set  mood and come up with the right sentence.
-        <strong className="warning" > Don't be lazy enough to tell direct text to your wife.</strong>
+        Love your wife! but her love language is words of affirmation, and you're not sure where to start? Use this generator to set your mood and come up with the right affirmation.
       </p>
-      <p className="small">A small help from a techie to the world</p>
+      <p><strong className="warning" > Don't be lazy enough to tell direct text to your wife.</strong></p>
+      <p className="small">** A small help from a techie to the world **</p>
       <p className="Quote">{currentQuote}</p>
       <button onClick={generateRandomQuote} className='quoteButton'>Set My Mood</button>
     </div>
